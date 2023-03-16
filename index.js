b16a0a36002fc3cd3976fbc1aec490051b86cd12
JTande99 <102241268+JTande99@users.noreply.github.com>
2023-03-16 11:09:12 -0700

    this one gave me a really bad headache

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function openMenu() {
     document.querySelector('.nav__menu--card').style.display = 'flex';
+    document.querySelector('.nav__menu--button').style.display = 'none';
+    document.querySelector('.nav__menu--close').style.display = 'initial';
 }
 
 function closeMenu() {
-    document.body.remove('menu--open')
+    document.querySelector('.nav__menu--card').style.display = 'none';
+    document.querySelector('.nav__menu--button').style.display = 'block';
+    document.querySelector('.nav__menu--close').style.display = 'none';
 }
 
 console.log('Hello sddssdkhjbyhuigds')
